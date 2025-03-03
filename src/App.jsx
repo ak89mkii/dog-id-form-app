6fcb9ee8ac62b30ad49599b29c10161a1614fad1
Alexander Kasem <akasem@tlit63009.com>
2025-03-03 10:58:21 -0500

    Fixed print bug.

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
     <div className="container">
       
       <h1>Dog ID Form Template App</h1>
-      <p>Developed by Ponder Code</p>
-      <p>NOTE: To easily resize or crop an image, go to <a href="https://www.resizepixel.com" target="_blank">ResizePixel.com</a> (not affiliated with Ponder Code)</p>
+      <p className="text">Developed by Ponder Code</p>
+      <p className="text">NOTE: To easily resize or crop an image, go to <a href="https://www.resizepixel.com" target="_blank">ResizePixel.com</a> (not affiliated with Ponder Code)</p>
       <button onClick={handlePrint} className="print-button">Print ID Card</button>
       <br></br>      
       <input type="file" accept="image/*" onChange={handleImageUpload} className="input-field" />
